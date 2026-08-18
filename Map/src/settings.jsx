019409d003524667ffddc0610c51d14960c53fd9
Garrik <garrikvertsman@gmail.com>
2026-08-18 10:54:14 -0400

    added points to the settings page

--- Map/src/settings.jsx
+++ Map/src/settings.jsx
@@ -1,7 +1,33 @@
-export default function Home() {
+import { supabase } from './supabaseClient';
+import { useEffect, useState } from 'react';
+
+export default function Settings() {
+    const [points, setPoints] = useState(0)
+    useEffect(() => {
+        async function getPoints() {
+            const { data: { user }, error } = await supabase.auth.getUser();
+            if (error) {
+                console.log(error)
+            }
+            else {
+                const { data, error } = await supabase.from('profiles')
+                    .select('points')
+                    .eq('id', user.id)
+                    .single()
+                if (error) {
+                    console.log(error)
+                }
+                else {
+                    setPoints(data.points)
+                }
+            }
+        }
+        getPoints()
+    }, [])
     return (
         <h1>
-            Settings
+            <h1> Settings </h1>
+            <p> Points {points} </p>
         </h1>
     )
 }
