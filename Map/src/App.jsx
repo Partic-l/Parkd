fb054c5b29c8116e77a38e7514f7f3bc6ac05520
Garrik <garrikvertsman@gmail.com>
2026-08-18 17:55:50 -0400

    add signup page

--- Map/src/App.jsx
+++ Map/src/App.jsx
@@ -4,11 +4,12 @@ import Settings from './settings';
 import Social from './social';
 import NavBar from './NavBar';
 import Login from './Login';
+import Signup from './Signup';
 import './App.css';
 
 function AppLayout() {
   const location = useLocation()
-  const hideNav = location.pathname === '/login'
+  const hideNav = location.pathname === '/login' || location.pathname === '/signup'
 
   return (
     <>
@@ -18,6 +19,7 @@ function AppLayout() {
         <Route path="/settings" element={<Settings />} />
         <Route path="/social" element={<Social />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </>
   )
---
+++ Map/src/Signup.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { supabase } from './supabaseClient';
+import { useNavigate } from "react-router";
+import './login.css'
+import logo from './assets/spaced.svg'
+
+export default function Signup() {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    async function handleSignUp() {
+        console.log('signing up with:', email, password)
+        const { data, error } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        })
+        if (error) {
+            console.log(error)
+        }
+        else {
+            navigate('/login')
+        }
+    }
+
+    return (
+        <div className="login-container">
+            <div className="logo">
+                <img src={logo} alt="Spaced" width="100" style={{ marginLeft: "10px" }} />
+                <span style={{ fontSize: "50px", marginLeft: "5px" }}>Spaced</span>
+            </div>
+            <div className="login">
+                <input type="text" className="text-input" placeholder="Email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" className="text-input" placeholder="Password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button className="btn" onClick={handleSignUp}>Sign Up</button>
+                <p>Already have an account? <a href="/login">Sign In!</a></p>
+            </div>
+        </div>
+    )
+}
